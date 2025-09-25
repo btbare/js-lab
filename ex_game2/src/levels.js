c73168ff731c77ef26d0ee1416aeb08c8d21e550
btbare <btbare2002@gmail.com>
2025-09-25 13:05:28 -0400

    make ex_game2 directory;

---
+++ ex_game2/src/levels.js
@@ -0,0 +1,7 @@
+export const level1 = `
+    <h1>level 1<h1>
+`
+
+export const level2 = `
+    <h1>level 2<h1>
+`
---
+++ ex_game2/src/main.js
@@ -0,0 +1,4 @@
+import {level1} from "./levels.js"
+import {level2} from "./levels.js"
+
+document.getElemenetById("app").innerHTML = level1
