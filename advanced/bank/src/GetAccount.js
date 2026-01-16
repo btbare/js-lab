2efb7fbe5079f9fa959b0b98c2acbcbe8f67cdf5
btbare15 <btbare2002@gmail.com>
2026-01-15 20:25:32 -0500

    GetAccount component works. UI needs improvement.
    fix:
    
    1) Button press sets input text/value to "[object Object]"
    
    2) Input ID's display before button press

--- advanced/bank/src/GetAccount.js
+++ advanced/bank/src/GetAccount.js
@@ -1,26 +1,29 @@
 import { useState } from "react";
 
-function GetAccount(){
+ function GetAccount(){
 
         const urlBase = "https://695f03af7f037703a8128fbf.mockapi.io/api/v1/Account"
-        const [accountID, setAccountID] = useState("")
+        let [accountID, setAccountID] = useState("")
         
-        var Submit = function() {
-            let req = fetch(urlBase+"/"+accountID)
-            let res = req.json()
+        var Submit = async function() {
+            let req = await fetch(urlBase+"/"+accountID)
+            let res = await req.json()
             setAccountID(res)
-            console.log(accountID)
         }
 
     return (
         <>
-            <input placeholder="Input desired account ID"
-                value = {accountID}
-                onChange = {Submit}
-            ></input>
+                <input placeholder="Input desired account ID"
+                    value={accountID}
+                    onChange={(e) => setAccountID(e.target.value) }
+                ></input>
 
-            <button onClick={Submit}>Submit</button>
-
+                <button 
+                    type = "button"
+                    onClick={Submit}
+                > Submit </button>  
+                              
+                {accountID ? (<p>{JSON.stringify(accountID)}</p>) : null}
         </>
     )
 }
